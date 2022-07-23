7d9fba8a678a6ffe41c4af5565808e683c41027f
Project <nileshsinha1731@gmail.com>
2022-07-24 01:19:27 +0530

    transform state to game history

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,40 @@ import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isNext, setIsNext] = useState(false);
+  const [history, setHistory] = useState([ {board: Array(9).fill(null), isXNext: true} ]);
+  //const [isNext, setIsNext] = useState(false);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const winner = calculateWinner(board);
+  const current = history[currentMove];
 
-  const message = winner ? `Winner is ${winner}` :  `Next player is ${isNext ? 'X' : 'O'}`;
+  console.log('history', history);
+
+  const winner = calculateWinner(current.board);
+
+  const message = winner ? `Winner is ${winner}` :  `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   console.log(winner);
 
   const handleSquareClick = position => {
-    if(board[position] || winner) return;
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    if(current.board[position] || winner) return;
+
+
+    setHistory(prev => {
+      const last = prev[prev.length-1]; //yields the last element in the previous array
+
+      const newBoard = last.board.map((square, pos) => {
         if(pos === position)
-          return isNext? 'X' : 'O';
+          return last.isXNext? 'X' : 'O';
 
         return square;
 
         });
+
+        return prev.concat([ {board: newBoard, isNext: !last.isXNext }])
       });
 
-      setIsNext(prev => !prev);
+      //setIsNext(prev => !prev);
+      setCurrentMove(prev => prev + 1); //update currentMove to let the game run
     };
 
 
@@ -34,7 +46,7 @@ return(
   <div className='app'>
     <h1>TIC TAC TOE</h1>
     <h2>{message}</h2>
-    <Board board={board} handleSquareClick={handleSquareClick}/>
+    <Board board={current.board} handleSquareClick={handleSquareClick}/>
   </div>
 )};
 
